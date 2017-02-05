2ea904505eb2d66decf2598ea1f26f08e3580d38
cloud168 <davecarlodarilag@gmail.com>
2017-02-05 12:46:21 +0800

    Update install.pp

--- install.pp
+++ install.pp
@@ -1,32 +1,38 @@
 class vim {
     
     package { "vim": 
-        ensure => installed 
+        ensure => installed,
     }
 
     service { "vim":
         ensure => running,
+        
+        require => package["vim"],
     }
 }
 
 class curl  {
 
     package { "curl":
-        ensure => installed
+        ensure => installed,
     }
         
     service { "curl":
         ensure => running,
+        
+        require => package["curl"],
     }
     
  class curl  {
 
     package { "git":
-        ensure => installed
+        ensure => installed,
     }
         
     service { "git":
         ensure => running,
+        
+        require => package["git"],
     }
         
   
